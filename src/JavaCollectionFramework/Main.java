59ec3db3b53a4b30b1922b3b75995c5539837bdd
gar13177 <gar13177@uvg.edu.gt>
2014-09-02 08:02:14 -0600

    Por imbecil le puse el main al set

--- src/JavaCollectionFramework/Main.java
+++ src/JavaCollectionFramework/Main.java
@@ -1,5 +1,7 @@
 package JavaCollectionFramework;
 
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,8 +17,31 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args){
+        int _opcionFactory;
+        int _opcionMenu = 0;
+        Scanner in  = new Scanner(System.in);
+        
+        System.out.println("Seleccione la implementacion del SET");
+        System.out.println(" 1. HashSet");
+        System.out.println(" 2. TreeSet");
+        System.out.println(" 3. LinkedHashSet");
+       
+        _opcionFactory = in.nextInt();
+        
+        while (_opcionMenu != 8){
+            System.out.println("Seleccione una opcion");
+            System.out.println(" 1. Agregar una persona");
+            System.out.println(" 2. Desarrolladores con experiencia en Java, web y celulares");
+            System.out.println(" 3. Desarrolladores con experiencia en Java pero no en web");
+            System.out.println(" 4. Desarrolladores con experiencia web y celulares, pero que no tengan experiencia en Java");
+            System.out.println(" 5. Desarrolladores con experiencia web o celulares, pero que no tengan experiencia en Java");
+            System.out.println(" 6. Indicar si el conjunto de desarrolladores Java es un subconjunto de Desarrolladores Web");
+            System.out.println(" 7. El conjunto que tenga la cantidad más grande de desarrolladores");
+            System.out.println(" 8. Desplegar integrantes del conjunto en 7");
+        }
+        
+        
     }
     
 }
--- src/JavaCollectionFramework/SetFactory.java
+++ src/JavaCollectionFramework/SetFactory.java
@@ -6,36 +6,10 @@
 
 package JavaCollectionFramework;
 
-import java.util.Scanner;
 /**
  *
  * @author Kevin
  */
 public class SetFactory {
-    public static void main(String[] args){
-        int _opcionFactory;
-        int _opcionMenu = 0;
-        Scanner in  = new Scanner(System.in);
-        
-        System.out.println("Seleccione la implementacion del SET");
-        System.out.println(" 1. HashSet");
-        System.out.println(" 2. TreeSet");
-        System.out.println(" 3. LinkedHashSet");
-       
-        _opcionFactory = in.nextInt();
-        
-        while (_opcionMenu != 8){
-            System.out.println("Seleccione una opcion");
-            System.out.println(" 1. Agregar una persona");
-            System.out.println(" 2. Desarrolladores con experiencia en Java, web y celulares");
-            System.out.println(" 3. Desarrolladores con experiencia en Java pero no en web");
-            System.out.println(" 4. Desarrolladores con experiencia web y celulares, pero que no tengan experiencia en Java");
-            System.out.println(" 5. Desarrolladores con experiencia web o celulares, pero que no tengan experiencia en Java");
-            System.out.println(" 6. Indicar si el conjunto de desarrolladores Java es un subconjunto de Desarrolladores Web");
-            System.out.println(" 7. El conjunto que tenga la cantidad más grande de desarrolladores");
-            System.out.println(" 8. Desplegar integrantes del conjunto en 7");
-        }
-        
-        
-    }
+    
 }
